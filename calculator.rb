a2db3baaab51f4b7bb8273f5b6b8555ae6d62dec
Joseph Stalnaker <josephlstalnaker@gmail.com>
2019-10-03 02:53:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,10 @@
 first_number = 0
 second_number = 1
 
-sum = first_number+second_number
+sum == first_number+second_number
 
-sum = first_number-second_number
+sum == first_number-second_number
 
-sum = first_number*second_number
+sum == first_number*second_number
 
-sum = first_number%second_number
+sum == first_number%second_number
